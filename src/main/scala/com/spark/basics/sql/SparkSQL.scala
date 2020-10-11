9ed01e3beeceda8a992c5ca666bd6d865e3adb4f
Lekkala <878127@cognizant.com>
2020-10-11 20:25:08 +0800

    Add Spark SQL

--- src/main/scala/com/spark/basics/sql/SparkSQL.scala
+++ src/main/scala/com/spark/basics/sql/SparkSQL.scala
@@ -40,14 +40,55 @@ object SparkSQL extends App {
     "password" -> "docker",
   )
 
-  def transferTable(tables: List[String]) = tables foreach { tableName =>
+  def transferTable(tables: List[String]): Unit = tables foreach { tableName =>
     val tableDF = spark.read.format("jdbc").options(postgres + ("dbtable" -> s"public.$tableName")).load()
     tableDF.createOrReplaceTempView(tableName)
-    tableDF.write
-      .mode(SaveMode.Overwrite)
-      .saveAsTable(tableName)
+    //tableDF.write.mode(SaveMode.Ignore).saveAsTable(tableName)
   }
 
   transferTable(List("employees", "salaries", "dept_manager", "titles"))
 
+  spark.read.table("employees")
+    //.show
+
+  /**
+   * Exercises
+   */
+
+  /**
+   * 1. Read the movies DF and store it as a Spark table in the rtjvm database
+   */
+
+  spark.read.option("inferSchema", "true").json("src/main/resources/data/movies.json")
+    .write.mode(SaveMode.Overwrite)
+   // .saveAsTable("movies")
+
+  spark.read
+    //.table("movies")
+    //.show
+
+  /**
+   * 2. Count how many employees were hired in between Jan 1 2000 and Jan 1 2001 (output = 1)
+   */
+  spark.read.table("employees")
+    .sqlContext
+    .sql(
+      """
+        | select count(*) from employees
+        | where hire_date >= CAST('2000-01-01' AS date) AND hire_date  <= CAST('2001-01-01' AS date)
+        |""".stripMargin)
+    // .show
+
+  /**
+   * 3. Show the average salaries for the employees hired in between those dates, grouped by department
+   */
+  spark.sql(
+    """
+      | select de.dept_no, avg(sal.salaries)
+      | from employees e,  dept_emp de, salaries sal
+      | where hire_date >= CAST('2000-01-01' AS date) AND hire_date  <= CAST('2001-01-01' AS date)
+      | and e.emp_no = de.emp_no and e.emp_no = sal.emp_no
+      | group by de.dept_no
+      |""".stripMargin)
+  //  .show
 }
